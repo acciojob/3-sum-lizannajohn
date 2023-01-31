379c95bb7be6a2a4339717dcf376b40fa0db38f0
lizannajohn <lizannabeth.jpg@gmail.com>
2023-01-31 17:41:10 +0400

    3 sum solved

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 function threeSum(arr, target) {
-  //your code here
+    //your code here
+    let closeSum = Number.MAX_VALUE;
+
+    for(let i = 0; i < arr.length; i++)
+    {
+        for(let j = i + 1; j < arr.length; j++)
+        {
+            for(let k = j + 1; k < arr.length; k++)
+            {
+                if(Math.abs(target - closeSum) > Math.abs(target - (arr[i] + arr[j] + arr[k])))
+                  closeSum = (arr[i] + arr[j] + arr[k])
+            }
+        }
+    }
+
+    return closeSum
 }
 
 module.exports = threeSum;
